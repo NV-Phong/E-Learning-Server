7a018411f44b0ea9617c4ed344a2ba058d22604b
NV-Phong <0h9ng03@gmail.com>
2025-08-20 15:09:10 +0700

    CONF - env config & hot reload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      ConfigModule.forRoot({
+         envFilePath: ['.env.development', '.env'],
+         isGlobal: true,
+      }),
+   ],
+   controllers: [AppController],
+   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+      origin: (origin, callback) => {
+         if (!origin || origin.startsWith(process.env.CLIENT_CORS)) {
+            callback(null, true);
+         } else {
+            callback(new Error('Not allowed by CORS'));
+         }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+   });
+   await app.listen(process.env.PORT || 3000);
+   Logger.log(
+      `🚀 API-Gateway is running on port http://localhost:${process.env.PORT} 🚀`,
+   );
+
+   if (module.hot) {
+      module.hot.accept();
+      module.hot.dispose(() => app.close());
+   }
 }
 bootstrap();
