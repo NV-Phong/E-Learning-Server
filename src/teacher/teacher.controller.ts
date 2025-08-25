8aabe4ff9e3c98c0ffa9db349acb0d34b5fb1711
NV-Phong <0h9ng03@gmail.com>
2025-08-26 00:27:56 +0700

    FEAT - teacher controllers

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,8 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Put,
+   Delete,
+   Param,
+   Body,
+} from '@nestjs/common';
 import { TeacherService } from './teacher.service';
-import { Teacher } from '@app/types';
+import { Teacher } from './teacher.schema';
 
-@Controller('teacher')
+@Controller('teachers')
 export class TeacherController {
    constructor(private readonly teacherService: TeacherService) {}
+
+   @Post()
+   create(@Body() data: Partial<Teacher>) {
+      return this.teacherService.create(data);
+   }
+
+   @Get()
+   findAll() {
+      return this.teacherService.findAll();
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.teacherService.findOne(id);
+   }
+
+   @Put(':id')
+   update(@Param('id') id: string, @Body() data: Partial<Teacher>) {
+      return this.teacherService.update(id, data);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.teacherService.remove(id);
+   }
 }
