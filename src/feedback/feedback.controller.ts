48d7d442b22e1ede1d4fec6a1b6e176c1cd38ba1
NV-Phong <0h9ng03@gmail.com>
2025-08-26 00:28:38 +0700

    FEAT - feedback controllers

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Put,
+   Delete,
+   Param,
+   Body,
+} from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
+import { Feedback } from './feedback.schema';
 
-@Controller('feedback')
+@Controller('feedbacks')
 export class FeedbackController {
    constructor(private readonly feedbackService: FeedbackService) {}
+
+   @Post()
+   create(@Body() data: Partial<Feedback>) {
+      return this.feedbackService.create(data);
+   }
+
+   @Get()
+   findAll() {
+      return this.feedbackService.findAll();
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.feedbackService.findOne(id);
+   }
+
+   @Put(':id')
+   update(@Param('id') id: string, @Body() data: Partial<Feedback>) {
+      return this.feedbackService.update(id, data);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.feedbackService.remove(id);
+   }
 }
