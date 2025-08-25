4419b3d8d9df94954fe8809b0e2739b17472db27
NV-Phong <0h9ng03@gmail.com>
2025-08-26 00:29:04 +0700

    FEAT - transaction controllers

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Put,
+   Delete,
+   Param,
+   Body,
+} from '@nestjs/common';
 import { TransactionService } from './transaction.service';
+import { Transaction } from './transaction.schema';
 
-@Controller('transaction')
+@Controller('transactions')
 export class TransactionController {
    constructor(private readonly transactionService: TransactionService) {}
+
+   @Post()
+   create(@Body() data: Partial<Transaction>) {
+      return this.transactionService.create(data);
+   }
+
+   @Get()
+   findAll() {
+      return this.transactionService.findAll();
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.transactionService.findOne(id);
+   }
+
+   @Put(':id')
+   update(@Param('id') id: string, @Body() data: Partial<Transaction>) {
+      return this.transactionService.update(id, data);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.transactionService.remove(id);
+   }
 }
