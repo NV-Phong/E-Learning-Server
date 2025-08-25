8edb6a57ece455c05290dab9dd4b75ef8f755302
NV-Phong <0h9ng03@gmail.com>
2025-08-26 00:28:17 +0700

    FEAT - student controllers

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Put,
+   Delete,
+   Param,
+   Body,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
+import { Student } from './student.schema';
 
-@Controller('student')
+@Controller('students')
 export class StudentController {
    constructor(private readonly studentService: StudentService) {}
+
+   @Post()
+   create(@Body() data: Partial<Student>) {
+      return this.studentService.create(data);
+   }
+
+   @Get()
+   findAll() {
+      return this.studentService.findAll();
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.studentService.findOne(id);
+   }
+
+   @Put(':id')
+   update(@Param('id') id: string, @Body() data: Partial<Student>) {
+      return this.studentService.update(id, data);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.studentService.remove(id);
+   }
 }
