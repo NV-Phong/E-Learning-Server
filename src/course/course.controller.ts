1933da63bd4d660239cfa117821425c6ebe3ac5b
NV-Phong <0h9ng03@gmail.com>
2025-08-26 00:27:32 +0700

    FEAT - course controllers

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+   Controller,
+   Get,
+   Post,
+   Put,
+   Delete,
+   Param,
+   Body,
+} from '@nestjs/common';
 import { CourseService } from './course.service';
+import { Course } from './course.schema';
 
-@Controller('course')
+@Controller('courses')
 export class CourseController {
    constructor(private readonly courseService: CourseService) {}
+
+   @Post()
+   create(@Body() data: Partial<Course>) {
+      return this.courseService.create(data);
+   }
+
+   @Get()
+   findAll() {
+      return this.courseService.findAll();
+   }
+
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.courseService.findOne(id);
+   }
+
+   @Put(':id')
+   update(@Param('id') id: string, @Body() data: Partial<Course>) {
+      return this.courseService.update(id, data);
+   }
+
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.courseService.remove(id);
+   }
 }
